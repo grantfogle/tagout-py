1fa45fdf8e7a7cbadd487f17bc9464abb8de5ea6
grantfogle <grantjfogle@gmail.com>
2022-10-27 20:35:55 -0600

    adding tabula file

--- crawler.py
+++ crawler.py
@@ -1,5 +1,6 @@
-import tabula
+import camelot
+import tkinter
+print('cats')
+df = camelot.read_pdf("../../../../2021ElkDrawRecap-1.pdf", pages="all")
 
-df = tabula.read_pdf("../../../../2021ElkDrawRecap-1.pdf", pages="all")
-
-print(df)
+print(df[2])
