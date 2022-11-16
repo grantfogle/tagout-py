c56653e566b28a66d166da7d5dacc8fc35aec8fd
grantfogle <grantjfogle@gmail.com>
2022-11-15 19:46:44 -0700

    updating code in final crawler

--- finalCrawler.py
+++ finalCrawler.py
@@ -6,27 +6,36 @@ allTables = camelot.read_pdf("../../../../2021ElkDrawRecap-1.pdf", pages="all")
 
 allElkData = []
 currentHuntIndex = 0
+applicantData = ''
+successData = ''
 
 pattern = 'E[A-Z]\d\d\d[A-Z]\d[A-Z]'
-
 
 def getUnitStats():
     print('asdf')
 # for i in range(len(elkCodes)):
 # for i in range(10):
-def extractDataFromTables():
-    for table in allTables:
-        currentCode = elkCodes[currentHuntIndex]
-        print(currentCode)
+# def extractDataFromTables():
+for table in allTables:
+    currentCode = elkCodes[currentHuntIndex]
+    tableData = table.data[0][0]
+    print(currentCode)
+    print(table)
+    if 'Pre-Draw Applicants' in tableData:
+        applicantData = tableData
+    elif 'Post-Draw Successful' in tableData:
+        successData = tableData
 
-        # get applicant data
-        # get success data
-        # pass them to the above 
-        #if tableIncludes
         finalHuntObj = {
             [currentCode]: getUnitStats(applicantData, successData)
         }
+        
         allElkData.append(finalHuntObj)
+        currentHuntIndex += 1
+        # get applicant data
+        # get success data
+        # pass them to the above 
+        #if tableInclude
 # design for resuability for different species
 
 # go through each page
@@ -35,4 +44,4 @@ def extractDataFromTables():
 # go through the steps of recording the data from the page
 # if there is no elk code
 # go on
-extractDataFromTables()
+# extractDataFromTables()
