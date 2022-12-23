edb715fc237ddb3ab227d36b64b065ca5d449216
grantfogle <grantjfogle@gmail.com>
2022-12-22 22:40:34 -0700

    removing unneeded files, updating names

---
+++ crawlerMap.py
@@ -0,0 +1,10 @@
+# future implementation of automating these crawls
+
+coloradoMap = {
+    'species': 'elk',
+    'drawStatsInput': '../../../../2021ElkDrawRecap.pdf',
+    'drawStatsOutput': './output/colorado/2021-co-elk-draw-stats.json',
+    'unitStatsInput': 'tbd',
+    'unitStatsOutput': 'tbd',
+    'regex': 'E[A-Z]\d\d\d[A-Z]\d[A-Z]'
+}
