bb614a7c73b64fcdc1e808d8af0b292ccdf20da4
grantfogle <grantjfogle@gmail.com>
2023-11-14 18:01:47 -0700

    crawling pdf properly just need to extract text

--- colorado/population/population_module.py
+++ colorado/population/population_module.py
@@ -12,6 +12,23 @@ def extract_text_from_pdf(pdf_path):
 
     return text
 
+
 pdf_path = '../../data/colorado/population_estimates/2022ElkPopulationEstimates.pdf'
 extracted_text = extract_text_from_pdf(pdf_path)
-print(extracted_text)
+# print(extracted_text)
+
+extractedTextArr =  extracted_text.split('\n')
+for i in range(6, len(extractedTextArr) - 10):
+    splitTextArr = extractedTextArr[i].split(' ')
+    print(splitTextArr)
+    dau = splitTextArr[0]
+    populationEstimate = splitTextArr[-2]
+    maleFemaleRatio = splitTextArr[-1]
+    print(dau, populationEstimate, maleFemaleRatio)
+    # print(extractedTextArr[i].split(' '))
+    # index zero is the DAU
+    # index 2 -> 3/4 is the herd name
+    # other units are the GMU's
+    # index second to last is the post hunt estimate
+    # index last is bull cow ratio
+# print(extractedTextArr)
