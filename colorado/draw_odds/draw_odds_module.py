9828da44381d30594e278b894c2eed87573e89f9
grantfogle <grantjfogle@gmail.com>
2023-11-17 08:46:57 -0700

    adding first part of draw stats crawler, identifying draw units

--- colorado/draw_odds/draw_odds_module.py
+++ colorado/draw_odds/draw_odds_module.py
@@ -1,21 +1,42 @@
 from PyPDF2 import PdfReader
+import re
 
 input = '../../data/colorado/draw_reports/2023ElkDrawnOut.pdf'
 output = '../../data/colorado/draw_reports/outputs/2023_elk_draw_stats.json'
+pattern = r'E[EFM]\d{3}[A-Z]\d[A-Z]'
+
+def isHuntCode(line):
+    # regex match
+    print(line)
 
 def get_draw_odds(pdf_file):
     # read the pdf
     reader = PdfReader(pdf_file)
+    endPageText = 'Hunts shaded gray'
 
     for i in range(0, 1):
         page = reader.pages[i]
         pageText = page.extract_text()
         pageLines = pageText.splitlines()
-        for line in pageLines:
-            print(line)
-        
+        index = 0
+        found = False
+        beginDataCollection = False
 
-    print('get_draw_odds')
+        while index < len(pageLines):
+            if re.match(pattern, pageLines[index]):
+                print('match', pageLines[index])
+                beginDataCollection = True
+
+                while beginDataCollection:
+                    # iterate through
+                    beginDataCollection = False
+            else:
+                index += 1
+            if endPageText in pageLines[index]:
+                print('found')
+                found = True
+                break
+            # print(pageLines[index])
 
 
 
