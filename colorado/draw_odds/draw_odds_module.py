53f60bf229d026e3a009abc9bda853642bce1be2
grantfogle <grantjfogle@gmail.com>
2023-11-28 21:55:18 -0700

    updating draw stats method

--- colorado/draw_odds/draw_odds_module.py
+++ colorado/draw_odds/draw_odds_module.py
@@ -4,33 +4,83 @@ import re
 input = '../../data/colorado/draw_reports/2023ElkDrawnOut.pdf'
 output = '../../data/colorado/draw_reports/outputs/2023_elk_draw_stats.json'
 pattern = r'E[EFM]\d{3}[A-Z]\d[A-Z]'
+endPageText = 'Hunts shaded gray'
+firstPage = 0
+lastPage = 1
+
 
 def isHuntCode(line):
     # regex match
-    print(line)
+    return re.match(pattern, line)
+
+def isEndOfPage(line):
+    return re.match(endPageText, line)
 
 def get_draw_odds(pdf_file):
     # read the pdf
     reader = PdfReader(pdf_file)
-    endPageText = 'Hunts shaded gray'
 
-    for i in range(0, 1):
+    for i in range(firstPage, lastPage):
         page = reader.pages[i]
         pageText = page.extract_text()
         pageLines = pageText.splitlines()
         index = 0
-        found = False
-        beginDataCollection = False
+        endOfPage = False
 
-        while index < len(pageLines):
-            if re.match(pattern, pageLines[index]):
-                print('match', pageLines[index])
-                beginDataCollection = True
+        while index < len(pageLines) and endOfPage == False:
+            currentLine = pageLines[index]
+            collectDrawStats = False
+            count = 2
 
-                while beginDataCollection:
-                    # iterate through the next x amount of lines
-                    ## look for keywords, whether a unit is a preference unit or choice 2/3 or if it made it
-                    ## to leftover draw
+            if isHuntCode(currentLine):
+                huntCode = currentLine
+                listType = pageLines[index + 1]
+                collectDrawStats = True
+                residentUnitData = {
+                    'state': 'CO',
+                    'species': huntCode[0],
+                    'gender': huntCode[1],
+                    'unit': huntCode[2:5],
+                    'season': huntCode[5:7],
+                    'method': huntCode[7],
+                    'list_type': listType,
+                    'resident_status': 'resident',
+                    'draw_odds': {
+                        'firstChoice': [],
+                        'secondChoice': [],
+                        'thirdChoice': [],
+                    }
+                }
+                nonResidentUnitData = {
+                    'state': 'CO',
+                    'species': huntCode[0],
+                    'gender': huntCode[1],
+                    'unit': huntCode[2:5],
+                    'season': huntCode[5:7],
+                    'method': huntCode[7],
+                    'list_type': listType,
+                    'resident_status': 'non_resident',
+                    'draw_odds': {
+                        'firstChoice': [],
+                        'secondChoice': [],
+                        'thirdChoice': [],
+                    }
+                }
+                while collectDrawStats:
+                    print('count:', count)
+                    # keeps track of current index in selected hunt code
+
+                    tableData = pageLines[index + count]
+
+                    if isHuntCode(tableData) or isEndOfPage(tableData):
+                        collectDrawStats = False
+                        index += count
+                        count = 2
+                        found = True
+                    
+                    print(tableData)
+
+                    count += 1
                     ## if it made it to preference point draw only, then everything else is 0% draw odds
                     ## if it made it to choice 2 then pref points is 100%
                     ## if it made it to choice 3 then pref points is 100% and choice 2 is 100%
@@ -38,14 +88,12 @@ def get_draw_odds(pdf_file):
 
                     ## we will return objects in the following data format
                     # { state: , unit: , season: , huntCode: , prefPoints: , choice2: , choice3: , leftover: }
-                    beginDataCollection = False
+                    # if re.match(pattern, pageLines[index]):
+                    #     beginDataCollection = False
+                    #     index += count
+                index += 1
             else:
                 index += 1
-            if endPageText in pageLines[index]:
-                print('found')
-                found = True
-                break
-            # print(pageLines[index])
 
 
 
